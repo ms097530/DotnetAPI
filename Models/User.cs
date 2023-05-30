c94fbf943f48c58869997fa9291594589216043e
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-29 22:46:46 -0400

    updated user model

--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,7 @@ public partial class User
     public int UserId { get; set; }
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
+    public string Gender { get; set; } = "";
     public string Email { get; set; } = "";
     public bool Active { get; set; }
 
