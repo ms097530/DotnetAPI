f152944ea427c1e5af0442738e89289b0540071d
Michael Schultz <mikewschultz9029@gmail.com>
2023-07-16 12:16:52 -0400

    added UserRepository - lec 68

---
+++ Data/UserRepository.cs
@@ -0,0 +1,45 @@
+namespace DotnetAPI.Data
+{
+    public class UserRepository
+    {
+        DataContextEF _entityFramework;
+        public UserRepository(IConfiguration config)
+        {
+            _entityFramework = new DataContextEF(config);
+
+        }
+
+        public bool SaveChanges()
+        {
+            return _entityFramework.SaveChanges() > 0;
+        }
+
+        public void AddEntity<T>(T entityToAdd)
+        {
+            if (entityToAdd != null)
+            {
+                _entityFramework.Add(entityToAdd);
+            }
+        }
+
+        // * alternative that returns whether or not add was successful
+        // public bool AddEntity<T>(T entityToAdd)
+        // {
+        //     if (entityToAdd != null)
+        //     {
+        //         _entityFramework.Add(entityToAdd);
+        //         return true;
+        //     }
+        //     return false;
+        // }
+
+        public void RemoveEntity<T>(T entityToAdd)
+        {
+            if (entityToAdd != null)
+            {
+                // * don't need _entity.Framework.Users.Remove(entityToRemove) -> can use generically like below
+                _entityFramework.Remove(entityToAdd);
+            }
+        }
+    }
+}
